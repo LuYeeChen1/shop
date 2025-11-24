196491c68099909d1ee228898a89fee023ff3def
LuYeeChen <luyeechen@03gmail.com>
2025-11-24 14:23:50 +0800

    added and updated properties

--- src/main/java/com/backend/shop/Config/S3Config.java
+++ src/main/java/com/backend/shop/Config/S3Config.java
@@ -3,6 +3,8 @@ package com.backend.shop.Config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
@@ -22,11 +24,21 @@ public class S3Config {
      *
      * This is the safest and most recommended approach for EC2 and local development.
      */
-    @Bean
+    @Bean("s3Client")
+    @Profile("local")
     public S3Client s3Client() {
         return S3Client.builder()
                 .region(Region.of(region))
                 .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
+                .build();
+    }
+
+    @Bean("s3Client")
+    @Profile("dev")
+    public S3Client s3ClientDev() {
+        return S3Client.builder()
+                .region(Region.of(region))
+                .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
     }
 }
